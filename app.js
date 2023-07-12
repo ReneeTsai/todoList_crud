09afc58e3f7ea618f006b3e73b4cb1554d6a8b15
Rachel <juoruel@gmail.com>
2023-07-12 17:16:34 +0800

    add handlebars

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const exphbs = require("express-handlebars");
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
@@ -17,9 +18,12 @@ db.on("error", () => {
 db.once("open", () => {
   console.log("mongodb connected!");
 });
+//handlebars set view engine
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
 //--server------------------------------------
 app.get("/", (req, res) => {
-  res.send("this is todo-list server");
+  res.render("index");
 });
 app.listen("3000", () => {
   console.log("this is listening on localhost:3000");
