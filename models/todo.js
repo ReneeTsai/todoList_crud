7ea0d7e0c2397333643c208ade301ac213b01c1b
Rachel <juoruel@gmail.com>
2023-07-12 16:45:09 +0800

    add seeds todoSeeder to create database

--- models/todo.js
+++ models/todo.js
@@ -1,14 +1,13 @@
 const mongoose = require("mongoose");
-const { stringify } = require("querystring");
 const Schema = mongoose.Schema;
 const todoSchema = new Schema({
   name: {
-    type: String,
-    required: true,
+    type: String, // 資料型別是字串
+    required: true, // 這是個必填欄位
   },
   done: {
     type: Boolean,
   },
 });
 
-module.exports = mongoose.model("Todo", todoschema);
+module.exports = mongoose.model("Todo", todoSchema);
