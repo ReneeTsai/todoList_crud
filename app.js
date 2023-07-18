711d62ec3743f52a7d05d1f365dc4aa7d5a8c996
Rachel <juoruel@gmail.com>
2023-07-19 00:20:20 +0800

    heroku init

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 const routes = require("./routes"); // 引用路由器
 const app = express();
+const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 require("./config/mongoose");
 //app.use
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,6 +19,6 @@ app.use(routes); //將 request 導入路由器
 //新增資料的頁面搬移至routes.module.todo.js
 
 //監聽localhost:3000
-app.listen("3000", () => {
-  console.log("this is listening on localhost:3000");
+app.listen(PORT, () => {
+  console.log(`this is listening on localhost:${PORT}`);
 });
