d7898ea6163ecd6a19243064ccbc83a714698ac1
Rachel <juoruel@gmail.com>
2023-07-18 16:55:55 +0800

    add bootstrap

--- app.js
+++ app.js
@@ -1,29 +1,10 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const exphbs = require("express-handlebars");
-const Todo = require("./models/todo");
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
-// 引用路由器
-const routes = require("./routes");
-
-// 加入這段 code, 僅在非正式環境時, 使用 dotenv
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
+const routes = require("./routes"); // 引用路由器
 const app = express();
-//設定連線到mongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-// 取得資料庫連線狀態
-const db = mongoose.connection;
-//連線異常
-db.on("error", () => {
-  console.log("mongodb error");
-});
-//連線成功
-db.once("open", () => {
-  console.log("mongodb connected!");
-});
+require("./config/mongoose");
 //app.use
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
