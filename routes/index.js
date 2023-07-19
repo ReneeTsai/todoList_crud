104645c6a8ec64f94a98d4be0595e653feaf8d77
Rachel <juoruel@gmail.com>
2023-07-19 19:09:34 +0800

    add users.js for login

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const home = require("./module/home");
 const todos = require("./module/todos");
+const users = require("./module/users");
 
 router.use("/", home);
 router.use("/todos", todos);
+router.use("/users", users);
 module.exports = router;
