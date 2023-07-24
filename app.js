6381cd6a843954ef9dbe786a1ea84375e4863fb2
Rachel <juoruel@gmail.com>
2023-07-24 13:21:35 +0800

    add bcrypt.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const methodOverride = require("method-override");
 const routes = require("./routes"); // 引用路由器
 const app = express();
 const PORT = process.env.PORT || 3000;
+const flash = require("connect-flash"); // 引用套件
 // config passport.js
 const usePassport = require("./config/passport");
 // config mongoose.js
@@ -26,11 +27,15 @@ app.set("view engine", "hbs");
 //--server------------------------------------
 //
 usePassport(app);
+//提示成功、錯誤字元
+app.use(flash());
 //usePassport(app) 之後、app.use(routes) 之前，加入middleware：
 app.use((req, res, next) => {
   // 你可以在這裡 console.log(req.user) 等資訊來觀察
   res.locals.isAuthenticated = req.isAuthenticated();
   res.locals.user = req.user;
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.warning_msg = req.flash("warning_msg");
   next();
 });
 //app.use(routes); 取代上面app.get("/", (req, res)
